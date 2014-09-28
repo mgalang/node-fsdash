dbf7ac856f13bdd55d49159011942535ff3087cc
Marc Galang <marchenrygalang@gmail.com>
2014-09-28 15:59:24 +0300

    Add dashboard site

---
+++ site/app.js
@@ -0,0 +1,55 @@
+var express = require('express');
+var path = require('path');
+var storage = require(path.join(__dirname, '../lib/storage'));
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Node FS Dashboard' });
+});
+
+app.get('/logs', function(req, res) {
+  storage.getAll(function(items){
+    res.json(items);
+  });
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
---
+++ site/public/javascripts/main.js
@@ -0,0 +1,77 @@
+(function(Backbone, $, exports){
+  /*
+   * Logs Collection
+   */
+  var Logs = Backbone.Collection.extend({
+    url: '/logs'
+  });
+  
+  exports.logs = new Logs();
+
+  // custom template interpolation
+  _.templateSettings = {
+    interpolate: /\{\{(.+?)\}\}/g
+  };
+  
+  /*
+   * Log Item Row View
+   */
+  var LogItem = Backbone.View.extend({
+    tagName: 'tr',
+    initialize: function(params){
+      this.model = params.model || {};
+    },
+    template: _.template($('#logItemTpl').html()),
+    render: function(){
+      var attr = _.clone(this.model.attributes);
+
+      switch(attr.event){
+        case 'add':
+          attr.label_title = 'Added';
+          attr.class_suffix = '-success';
+          break;
+        case 'change':
+          attr.label_title = 'Changed';
+          attr.class_suffix = '-warning';
+          break;
+        case 'remove':
+          attr.label_title = 'Deleted';
+          attr.class_suffix = '-danger';
+          break;
+      }
+
+      this.$el.html(this.template(attr));
+      return this;
+    }
+  });
+
+  /*
+   * Log Items View
+   */
+  var LogItems = Backbone.View.extend({
+    el: '.logs-table',
+    initialize: function(){ 
+      logs.on('sync', this.render, this);
+    },
+    render: function(e){
+      var that = this;
+      this.$('tbody').empty();
+      logs.forEach(function(item){
+        
+        var logItem = new LogItem({ model: item });
+        that.$('tbody').append(logItem.render().el);
+      });
+    }
+  });
+
+  var App = function(){
+    new LogItems();
+
+    // fetch logs every second
+    setInterval(function(){
+      logs.fetch();
+    }, 1000);
+  };
+
+  new App();
+})(Backbone, Backbone.$, window);
